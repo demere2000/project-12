7ac846e3def0222c391317a754589a388bd8d50b
Demere Walker <blessedtrulywalker32@gmail.com>
2023-12-20 14:56:14 -0600

    Updated feedback for Quiz App

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
 $(document).ready(function() {
+    // Initialize variables for current question and total questions
     let currentQuestion = 1;
-    let totalQuestions = 3; // Set the total number of questions
+    let totalQuestions = 3;
 
+    // Function to display the next question
     function showNextQuestion() {
+        // Check if there are more questions to display
         if (currentQuestion <= totalQuestions) {
-            // Enable all options for the new question
+            // Enable all options and attach click event handler
             $('.option').removeClass('disabled').off('click').on('click', selectAnswer);
 
-            // Display the next question
+            // Clear feedback text
             $('#feedback').text('');
+
+            // Logic for each question
             if (currentQuestion === 1) {
                 $('#quiz h2').text('Question ' + currentQuestion + ': What is the capital of France?');
-                // Replace answer options for question 1
+                // Set options for question 1
                 $('.option:eq(0)').text('A) Berlin');
                 $('.option:eq(1)').text('B) London');
                 $('.option:eq(2)').text('C) Paris');
                 $('.option:eq(3)').text('D) Madrid');
             } else if (currentQuestion === 2) {
                 $('#quiz h2').text('Question ' + currentQuestion + ': What is the largest planet in our solar system?');
-                // Replace answer options for question 2
+                // Set options for question 2
                 $('.option:eq(0)').text('A) Earth');
                 $('.option:eq(1)').text('B) Mars');
                 $('.option:eq(2)').text('C) Jupiter');
                 $('.option:eq(3)').text('D) Venus');
             } else if (currentQuestion === 3) {
                 $('#quiz h2').text('Question ' + currentQuestion + ': Who wrote the play "Romeo and Juliet"?');
-                // Replace answer options for question 3
+                // Set options for question 3
                 $('.option:eq(0)').text('A) William Shakespeare');
                 $('.option:eq(1)').text('B) Charles Dickens');
                 $('.option:eq(2)').text('C) Mark Twain');
                 $('.option:eq(3)').text('D) Jane Austen');
             }
 
-            // Increment the current question
+            // Increment the current question number
             currentQuestion++;
         } else {
-            // Quiz is complete
+            // Display completion message when quiz is finished
             $('#quiz h2').text('Quiz Complete');
             $('#feedback').text('You have finished the quiz.');
         }
     }
 
+    // Function to handle answer selection
     function selectAnswer() {
-        // Disable all options to prevent further clicks for the current question
+        // Disable options to prevent multiple selections
         $('.option').addClass('disabled');
 
+        // Check if selected answer is correct
         if ($(this).text().includes('C) Paris') || $(this).text().includes('C) Jupiter') || $(this).text().includes('A) William Shakespeare')) {
-            // Correct answer
+            // Display correct feedback
             $('#feedback').text('Correct!').addClass('correct').removeClass('incorrect');
-            // Proceed to the next question
+            // Proceed to the next question after a delay
             setTimeout(showNextQuestion, 2000);
         } else {
-            // Incorrect answer
+            // Display incorrect feedback
             $('#feedback').text('Incorrect!').addClass('incorrect').removeClass('correct');
-            // Allow the user to attempt the same question again
+            // Re-enable options for user to try again
             $('.option').removeClass('disabled').off('click').on('click', selectAnswer);
         }
     }
